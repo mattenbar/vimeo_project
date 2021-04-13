beacafbff43543d1509191c9bf83787451ae5643
mattenbar <matthw.enbar@gmail.com>
2021-04-13 08:37:20 -0400

    removed comments

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import Speech from "react-speech";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  state = {
+    video: "",
+  };
+
+  componentDidMount() {
+
+    fetch("https://vimeo.com/api/oembed.json?url=https://vimeo.com/136392292")
+      .then((response) => response.json())
+      .then((data) =>
+        this.setState({
+          video: data,
+        })
+      );
+  }
+
+  render() {
+    
+    if (this.state.video) {
+ 
+      return (
+        console.log(this.state.video),
+        <div className="App">
+          <iframe
+            title="video"
+            src="https://player.vimeo.com/video/136392292"
+            width="640"
+            height="360"
+            frameBorder="0"
+            allow="autoplay; fullscreen; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+          <p>
+            <a href="https://vimeo.com/136392292">
+              The Knocks &quot;Collect My Love&quot; featuring Alex Newell
+            </a>{" "}
+            from <a href="https://vimeo.com/austinpeters">Austin Peters</a> on{" "}
+            <a href="https://vimeo.com">Vimeo</a>.
+          </p>
+          <Speech
+            textAsButton={true}
+            displayText="listen to title"
+            text={this.state.video.title}
+          />
+          <Speech
+            textAsButton={true}
+            displayText="listen to description"
+            text={this.state.video.description}
+          />
+        </div>
+      );
+    } else {
+      return <div></div>;
+    }
+  }
 }
 
 export default App;
